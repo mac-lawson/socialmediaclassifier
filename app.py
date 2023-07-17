796660bbc1acc24d968ed7a1667d10f94144b15d
mac lawson <macattackairman@gmail.com>
2023-07-17 15:31:22 -0400

    new webapp

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 
 @app.route('/', methods = ["GET", "POST"])
 def home():
-    # if request.method == "POST":
+    if request.method == "POST":
+        url_to_scan = str(request.form.get("urlinput"))
+        content = str(classify(url_to_scan))
+        print(content)
+        processor = Process(content)
+        return render_template("site/index.html", classifier=str(content), judgement=str(processor.sentiment(str(processor.judge()))))
+        
+    return render_template("site/index.html", classifier="", judgement="")
 
-    return render_template("index.html")
+if __name__ == "__main__":
+    app.run()
